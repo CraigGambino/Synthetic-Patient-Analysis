8e27fa9645e7088f6d1e5878cdf10592ab96117a
CraigGambino <gambinocraig@gmail.com>
2018-07-31 21:21:39 -0400

    working on ordering diagnoses chronologically

---
+++ ds_imp_set.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+np.set_printoptions(suppress=True, precision=4)
+pd.set_option('display.max_columns', None)
+sns.set_context(context='notebook')
+sns.set_style(style='whitegrid')
